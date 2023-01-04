27c29a7bdcc43e3783e18ddd4312ef10240c3c21
zdw <zdw0607@gmail.com>
2023-01-04 08:43:38 +0800

    algorithm

--- algorithm/src/main/java/com/xcode/icu/leetcode/_22/Solution.java
+++ algorithm/src/main/java/com/xcode/icu/leetcode/_22/Solution.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Solution {
 
     public List<String> generateParenthesis(int n) {
-        
 
+        return null;
     }
 }
--- common/xcode-util/src/main/java/com/xcode/util/BeanUtils.java
+++ common/xcode-util/src/main/java/com/xcode/util/BeanUtils.java
@@ -8,19 +8,24 @@ import org.springframework.objenesis.ObjenesisStd;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * @Author zhoudawei
+ * @Date 2023/1/3 16:08
+ * @Version 1.0
+ */
 public class BeanUtils {
 
     private BeanUtils() {
     }
 
-    private static ThreadLocal<ObjenesisStd> objenesisStdThreadLocal = ThreadLocal.withInitial(ObjenesisStd::new);
-    private static ConcurrentHashMap<Class<?>, ConcurrentHashMap<Class<?>, BeanCopier>> cache = new ConcurrentHashMap<>();
+    private static ThreadLocal<ObjenesisStd> OBJENESISSTD_THREADLOCAL = ThreadLocal.withInitial(ObjenesisStd::new);
+    private static ConcurrentHashMap<Class<?>, ConcurrentHashMap<Class<?>, BeanCopier>> CACHE = new ConcurrentHashMap<>();
 
     public static <T> T copy(Object source, Class<T> target) {
         if (Objects.isNull(source)) {
             return null;
         }
-        return copy(source, objenesisStdThreadLocal.get().newInstance(target));
+        return copy(source, OBJENESISSTD_THREADLOCAL.get().newInstance(target));
     }
 
     public static <T> T copy(Object source, T target) {
@@ -35,7 +40,7 @@ public class BeanUtils {
         }
 
         ArrayList<T> list = new ArrayList<>(sources.size());
-        ObjenesisStd objenesisStd = objenesisStdThreadLocal.get();
+        ObjenesisStd objenesisStd = OBJENESISSTD_THREADLOCAL.get();
         for (Object source : sources) {
             if (source == null) {
                 throw new RuntimeException("empty source object");
@@ -53,7 +58,7 @@ public class BeanUtils {
     }
 
     public static <T> T mapToBean(Map<?, ?> source, Class<T> target) {
-        T bean = objenesisStdThreadLocal.get().newInstance(target);
+        T bean = OBJENESISSTD_THREADLOCAL.get().newInstance(target);
         BeanMap beanMap = BeanMap.create(bean);
         beanMap.putAll(source);
         return bean;
@@ -64,7 +69,7 @@ public class BeanUtils {
     }
 
     private static <S, T> BeanCopier getCacheBeanCopier(Class<S> source, Class<T> target) {
-        ConcurrentHashMap<Class<?>, BeanCopier> copierConcurrentHashMap = cache.computeIfAbsent(source, aClass -> new ConcurrentHashMap<>(16));
+        ConcurrentHashMap<Class<?>, BeanCopier> copierConcurrentHashMap = CACHE.computeIfAbsent(source, aClass -> new ConcurrentHashMap<>(16));
         return copierConcurrentHashMap.computeIfAbsent(target, aClass -> BeanCopier.create(source, target, false));
     }
 
